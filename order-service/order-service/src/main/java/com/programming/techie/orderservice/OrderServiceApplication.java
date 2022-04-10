25a5db9677c4134bf5e10a552b4756d02e89838c
joseleii <Phambaodu25072001@gmail.com>
2022-04-10 17:31:53 +0700

    init

---
+++ order-service/order-service/src/main/java/com/programming/techie/orderservice/OrderServiceApplication.java
@@ -0,0 +1,49 @@
+package com.programming.techie.orderservice;
+
+//import feign.RequestInterceptor;
+import com.programming.techie.orderservice.controller.OrderController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.sleuth.instrument.async.TraceableExecutorService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@RequiredArgsConstructor
+@ComponentScan({"com.programming.techie.orderservice.client"})
+@ComponentScan(basePackageClasses = OrderController.class)
+public class OrderServiceApplication {
+
+    private final BeanFactory beanFactory;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+//    @Bean
+//    public RequestInterceptor requestTokenBearerInterceptor() {
+//        return requestTemplate -> {
+//            JwtAuthenticationToken token = (JwtAuthenticationToken) SecurityContextHolder.getContext()
+//                    .getAuthentication();
+//
+//            requestTemplate.header("Authorization", "Bearer " + token.getToken().getTokenValue());
+//        };
+//    }
+//
+    @Bean
+    public ExecutorService traceableExecutorService() {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        return new TraceableExecutorService(beanFactory, executorService);
+    }
+}
