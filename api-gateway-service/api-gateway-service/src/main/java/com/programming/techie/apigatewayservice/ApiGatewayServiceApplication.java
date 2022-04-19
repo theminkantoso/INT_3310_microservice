8f3735e68667732f995f3be1585275f493aef199
joseleii <Phambaodu25072001@gmail.com>
2022-04-19 11:08:25 +0700

    update

--- api-gateway-service/api-gateway-service/src/main/java/com/programming/techie/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/api-gateway-service/src/main/java/com/programming/techie/apigatewayservice/ApiGatewayServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@EnableEurekaClient
-//@RestController
+@EnableEurekaClient
+@RestController
 public class ApiGatewayServiceApplication {
 
     public static void main(String[] args) {
